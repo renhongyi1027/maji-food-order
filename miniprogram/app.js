f38f5444d5ef04bfd57c559ad85937f17d0297b8
renhongyi1027 <renhongyi1027@gmail.com>
2026-05-17 19:41:29 +0700

    修复：填写云开发环境 ID

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,9 +4,9 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
       wx.cloud.init({
-        // env: '您的环境ID', // 建议去云开发控制台复制环境ID填在这里
+        env: 'cloud1-d9gq7cyor42fa56b5',
         traceUser: true,
-        timeout: 10000 // 将超时时间延长至10秒
+        timeout: 10000
       });
     }
     this.globalData = {};
